b315562d0c7c8dd74584c8c3ec154ef9235cf4df
Samriddha11 <samriddhachoudhuri@gmail.com>
2024-03-28 11:34:04 +0530

    another toy triangle

--- toy-triangle2.py
+++ toy-triangle2.py
@@ -3,9 +3,9 @@ def iswhat(arms):
 	b=arms[1]
 	c=arms[2]
 	if a==b and b==c and c==a:
-		print("Equilateral")
+		print("Equi")
 	elif a==b or a==c or b==c:
-		print("Isoceles")
+		print("Iso")
 	else:
 		print("No Match")
 
@@ -17,3 +17,4 @@ def toy_triangle(string,num):
     #return newstring
 #toy_triangle("2 2 1",'3 3 3','6 5 7', 3)
 toy_triangle("221 333 657", 3)
+print("Done")
